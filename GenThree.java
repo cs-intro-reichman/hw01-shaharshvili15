25ffbac3181172a2d7495b7227b175932b8fe798
shaharshvili15 <shahar.shvili@post.runi.ac.il>
2023-12-19 21:41:42 +0200

    add GetThree class that contains a function that get min and max generates 3 random number between them and prints the minimal number that was generated

--- GenThree.java
+++ GenThree.java
@@ -2,8 +2,26 @@
  * Generates three random integers, each in a given range [a,b),
  * prints them, and then prints the minimal number that was generated.
  */
+import java.util.Random;
 public class GenThree {
 	public static void main(String[] args) {
-		// Put your code here	
+		// Put your code here
+		var min = Integer.parseInt(args[0]);
+		var max = Integer.parseInt(args[1]);
+		Random rand = new Random();
+		int firstRandom = rand.nextInt(min,max);
+		int minNumber = firstRandom;
+		System.out.println(firstRandom);
+		for(int i = 0 ; i<2; i++)
+		{
+			int random = rand.nextInt(min,max);
+			System.out.println(random);
+			if(random < minNumber)
+			{
+				minNumber = random;
+			}
+		}
+		System.out.printf("The minimal generated number was %d", minNumber);
+
 	}
 }
